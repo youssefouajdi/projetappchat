8bcf0578662321886c55d6729633e51f7ce2dc61
youssefouajdi <youssefouajdi1@gmail.com>
2020-04-14 01:48:02 +0200

    myapp

---
+++ app/src/main/java/com/example/mycarto/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.mycarto;
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private static final int PERMS_CALL_ID=1234;
+    private LocationManager lm;
+    private MapFragment mapFragment;
+    private GoogleMap googleMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getFragmentManager();
+        mapFragment =(MapFragment)fragmentManager.findFragmentById(R.id.map);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermissions();
+    }
+    private void checkPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION ,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, PERMS_CALL_ID);
+            return;
+        }
+
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        }
+        if( lm.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)){
+            lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER , 1000 , 0 ,this);
+        }
+        if( lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER , 1000 , 0 ,this);
+        }
+        loadMap();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==PERMS_CALL_ID){
+            checkPermissions();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(lm !=null) {
+            lm.removeUpdates(this);
+        }
+    }
+
+    private void loadMap(){
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                MainActivity.this.googleMap = googleMap;
+                googleMap.moveCamera(CameraUpdateFactory.zoomBy(15));
+                googleMap.setMyLocationEnabled(true);
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(43.55555,6.589852))
+                        .title("ma position"));
+            }
+        });
+
+    }
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude=location.getLatitude();
+        double longitude=location.getLongitude();
+
+        Toast.makeText(this,"Location :"+latitude+"/"+longitude,Toast.LENGTH_LONG).show();
+        if(googleMap!=null){
+            LatLng googleLocation =new LatLng(latitude,longitude);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(googleLocation));
+        }
+    }
+}
